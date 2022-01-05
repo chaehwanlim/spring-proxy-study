b8371fdb81309501552246f953c292c66b86ef28
chaehwanlim <chlim428@gmail.com>
2022-01-05 22:01:58 +0900

    feat: 스프링 컨테이너에 인터페이스 기반 프록시 객체를 등록한다

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,18 +1,25 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV1Config;
-import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1proxy.InterfaceProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import({AppV1Config.class, AppV2Config.class})
+@Import(InterfaceProxyConfig.class)
 // V1, V2, V3 바꿔가면서 적용해볼 거라서 임시로 컴포넌트 스캔 대상을 한정지음
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
+	}
+
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
 	}
 
 }
