90572288dfacd3b41eb51aee5f33875eff6ec6ff
chaehwanlim <chlim428@gmail.com>
2022-01-05 22:59:44 +0900

    feat: 스프링 컨테이너에 클래스 기반 프록시를 등록해본다

---
+++ src/main/java/hello/proxy/config/v1proxy/concreteproxy/OrderControllerConcreteProxy.java
@@ -0,0 +1,43 @@
+package hello.proxy.config.v1proxy.concreteproxy;
+
+import hello.proxy.app.v2.OrderControllerV2;
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+
+public class OrderControllerConcreteProxy extends OrderControllerV2 {
+
+    private final OrderControllerV2 target;
+
+    private final LogTrace logTrace;
+
+    public OrderControllerConcreteProxy(OrderControllerV2 target, LogTrace logTrace) {
+        super(null);    // 부모 OrderControllerV2의 기능을 사용하지 않는 프록시이기 때문
+        this.target = target;
+        this.logTrace = logTrace;
+    }
+
+    @Override
+    public String request(String itemId) {
+        TraceStatus status = null;
+
+        try {
+            status = logTrace.begin("OrderController.request()");
+
+            // target 호출
+            String result = target.request(itemId);
+
+            logTrace.end(status);
+
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+
+            throw e;
+        }
+    }
+
+    @Override
+    public String noLog() {
+        return target.noLog();
+    }
+}
---
+++ src/main/java/hello/proxy/config/v1proxy/concreteproxy/OrderServiceConcreteProxy.java
@@ -0,0 +1,23 @@
+package hello.proxy.config.v1proxy.concreteproxy;
+
+import hello.proxy.app.v2.OrderRepositoryV2;
+import hello.proxy.app.v2.OrderServiceV2;
+import hello.proxy.trace.logtrace.LogTrace;
+
+public class OrderServiceConcreteProxy extends OrderServiceV2 {
+
+    private final OrderServiceV2 target;
+
+    private final LogTrace logTrace;
+
+    public OrderServiceConcreteProxy(OrderServiceV2 target, LogTrace logTrace) {
+        super(null);    // 부모 OrderRepository의 기능을 사용하지 않는 프록시이기 때문에
+        this.target = target;
+        this.logTrace = logTrace;
+    }
+
+    @Override
+    public void orderItem(String itemId) {
+        super.orderItem(itemId);
+    }
+}
