e881f533cd6e682edaaae3f5edf528516e9185d7
chaehwanlim <chlim428@gmail.com>
2022-01-07 22:47:14 +0900

    feat: 동적 프록시를 사용해 Log 추적기를 적용해본다

---
+++ src/main/java/hello/proxy/config/v2dynamicproxy/DynamicProxyBasicConfig.java
@@ -0,0 +1,41 @@
+package hello.proxy.config.v2dynamicproxy;
+
+import hello.proxy.app.v1.*;
+import hello.proxy.config.v2dynamicproxy.handler.LogTraceBasicHandler;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Proxy;
+
+@Configuration
+public class DynamicProxyBasicConfig {
+
+    @Bean
+    public OrderControllerV1 orderControllerV1(LogTrace logTrace) {
+        OrderControllerV1Impl orderController = new OrderControllerV1Impl(orderServiceV1(logTrace));
+
+        LogTraceBasicHandler handler = new LogTraceBasicHandler(orderController, logTrace);
+
+        return (OrderControllerV1) Proxy.newProxyInstance(OrderControllerV1.class.getClassLoader(), new Class[]{OrderControllerV1.class}, handler);
+    }
+
+    @Bean
+    public OrderServiceV1 orderServiceV1(LogTrace logTrace) {
+        OrderServiceV1Impl orderService = new OrderServiceV1Impl(orderRepositoryV1(logTrace));
+
+        LogTraceBasicHandler handler = new LogTraceBasicHandler(orderService, logTrace);
+
+        return (OrderServiceV1) Proxy.newProxyInstance(OrderServiceV1.class.getClassLoader(), new Class[]{OrderServiceV1.class}, handler);
+    }
+
+    @Bean
+    public OrderRepositoryV1 orderRepositoryV1(LogTrace logTrace) {
+        OrderRepositoryV1Impl orderRepository = new OrderRepositoryV1Impl();
+
+        LogTraceBasicHandler handler = new LogTraceBasicHandler(orderRepository, logTrace);
+
+        return (OrderRepositoryV1) Proxy.newProxyInstance(OrderRepositoryV1.class.getClassLoader(), new Class[]{OrderRepositoryV1.class}, handler);
+    }
+
+}
